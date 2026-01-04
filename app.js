af2c56e1eee890e88176b7472135d33399a70446
javier garcia alvarez <javigarcial87@gmail.com>
2026-01-04 21:20:03 +0100

    appjs

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+
+const indexRouter = require('./routes/index');
+const endrouteRouter = require('./routes/endroute');
+
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/endroute', endrouteRouter);
+app.use('/', indexRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando en http://localhost:${PORT}`);
+});
